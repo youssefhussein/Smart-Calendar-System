7b1368e03683659151f9a5affcb1d188502d88b5
youssef hussein <20634930+youssefhussein@users.noreply.github.com>
2025-03-27 14:08:49 +0200

    fixed css links

--- src/main/java/com/schedule/calendar/Controllers/AuthController.java
+++ src/main/java/com/schedule/calendar/Controllers/AuthController.java
@@ -8,12 +8,19 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-@GetMapping("/login")
+    
+    @GetMapping("/login")
     public ModelAndView loginIndex() {
-    return new ModelAndView("/auth/login.html");
-}
-@GetMapping("/signup")
+        return new ModelAndView("/auth/login.html");
+    }
+    
+    @GetMapping("/signup")
     public ModelAndView signupIndex() {
-    return new ModelAndView("/auth/signup.html");
-}
+        return new ModelAndView("/auth/signup.html");
+    }
+    
+    @GetMapping("")
+    public ModelAndView fallback() {
+        return new ModelAndView("/auth/login.html");
+    }
 }
