c0bd2e45859a5980e7e29ae4d9ff3efa68d7f40d
youssef hussein <20634930+youssefhussein@users.noreply.github.com>
2025-04-06 00:47:14 +0200

    fix login and home page

--- src/main/java/com/schedule/calendar/Models/User.java
+++ src/main/java/com/schedule/calendar/Models/User.java
@@ -1,94 +1,94 @@
 package com.schedule.calendar.Models;
 
 import jakarta.persistence.*;
+
 import java.util.Objects;
 
 @Entity
 @Table(name = "users")
 public class User {
-
+    
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-   
     @Column(unique = true)
     private String username;
-
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserRole role = UserRole.STUDENT;
-
+    
     public User() {
     }
-
+    
     public User(String username, String email, String password, UserRole role) {
         this.username = username;
         this.email = email;
         this.password = password;
         this.role = role;
     }
-
+    
     public String getUsername() {
         return this.username;
     }
-
+    
     public void setUsername(String username) {
         this.username = username;
     }
-
+    
     public String getEmail() {
         return this.email;
     }
-
+    
     public void setEmail(String email) {
         this.email = email;
     }
-
+    
     public String getPassword() {
         return this.password;
     }
-
+    
     public void setPassword(String password) {
         this.password = password;
     }
-
+    
     public UserRole getRole() {
         return this.role;
     }
-
+    
     public void setRole(UserRole role) {
         this.role = role;
     }
-
+    
     public User username(String username) {
         setUsername(username);
         return this;
     }
-
+    
     public User email(String email) {
         setEmail(email);
         return this;
     }
-
+    
     public User password(String password) {
         setPassword(password);
         return this;
     }
-
+    
     public User role(UserRole role) {
         setRole(role);
         return this;
     }
-  public Long getId() {
+    
+    public Long getId() {
         return id;
     }
-
+    
     public void setId(Long id) {
         this.id = id;
     }
+    
     @Override
     public boolean equals(Object o) {
         if (o == this)
@@ -99,20 +99,20 @@ public class User {
         User user = (User) o;
         return Objects.equals(username, user.username) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(role, user.role);
     }
-
+    
     @Override
     public int hashCode() {
         return Objects.hash(username, email, password, role);
     }
-
+    
     @Override
     public String toString() {
         return "{" +
-            " username='" + getUsername() + "'" +
-            ", email='" + getEmail() + "'" +
-            ", password='" + getPassword() + "'" +
-            ", role='" + getRole() + "'" +
-            "}";
+                " username='" + getUsername() + "'" +
+                ", email='" + getEmail() + "'" +
+                ", password='" + getPassword() + "'" +
+                ", role='" + getRole() + "'" +
+                "}";
     }
-
+    
 }
